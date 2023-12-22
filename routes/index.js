4650990c66aaa5dbd040b8b486e2bccfb530b70f
Haseeb Jan Hamraz <64940162+haseebjanhamraz@users.noreply.github.com>
2023-12-23 00:40:05 +0500

    App is now getting all data according to ticket form

--- routes/index.js
+++ routes/index.js
@@ -1,75 +1,126 @@
 // routes/index.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Item = require('../models/item');
-const { upload } = require('../config/multer');  // Adjusted import
-const user = require('../models/User');
-const isAuthenticated = require('./protected'); // Update this line
-
-
+const Item = require("../models/item");
+const { upload } = require("../config/multer"); // Adjusted import
+const user = require("../models/User");
+const isAuthenticated = require("./protected"); // Update this line
 
 // Index route
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const user = req.user;
   const items = await Item.find();
-  res.render('index', { items, user: req.user });
+  res.render("index", { items, user: req.user });
 });
-
 
 // Handle form submission
-router.post('/add', isAuthenticated , upload.single('profileImage'), async (req, res) => {
-  try {
-    // Extract data from the form submission
-    const { name, fathername, email, district, constituencyType, constituency , cnic, message } = req.body;
+router.post(
+  "/add",
+  isAuthenticated,
+  upload.single("profileImage"),
+  async (req, res) => {
+    try {
+      // Extract data from the form submission
+      const {
+        name,
+        fathername,
+        contactNumber,
+        district,
+        constituencyType,
+        constituency,
+        cnic,
+        currentAddress,
+        permanentAddress,
+        suggesterName,
+        suggesterFatherName,
+        suggesterContactNum,
+        suggesterCNIC,
+        suggesterCurrentAddress,
+        suggConstTyp,
+        suggesterConstituency,
+        vindicatorName,
+        vindicatorFatherName,
+        vindicatorContactNum,
+        vindicatorCNIC,
+        vindicatorCurrentAddress,
+        vindConstTyp,
+        vindicatorConstituency,
+        cadidateSign,
+        suggesterSign,
+        vindicatorSign,
+        ucPresidentSign,
+        tehsilPresidentSign,
+        districtPresidentSign,
+      } = req.body;
 
-    // Get the uploaded image file
-    const profileImage = req.file;
+      // Get the uploaded image file
+      const profileImage = req.file;
 
-    // Create a new item instance
-    const newItem = new Item({  
-      name,
-      fathername,
-      email,
-      district,
-      constituencyType,
-      constituency,
-      cnic,
-      message,
-      question,
-      profileImage: profileImage.filename, // Save the filename to the database
-    });
+      // Create a new item instance
+      const newItem = new Item({
+        name,
+        fathername,
+        contactNumber,
+        district,
+        constituencyType,
+        constituency,
+        cnic,
+        currentAddress,
+        permanentAddress,
+        suggesterName,
+        suggesterFatherName,
+        suggesterContactNum,
+        suggesterCNIC,
+        suggesterCurrentAddress,
+        suggConstTyp,
+        suggesterConstituency,
+        vindicatorName,
+        vindicatorFatherName,
+        vindicatorContactNum,
+        vindicatorCNIC,
+        vindicatorCurrentAddress,
+        vindConstTyp,
+        vindicatorConstituency,
+        cadidateSign,
+        suggesterSign,
+        vindicatorSign,
+        ucPresidentSign,
+        tehsilPresidentSign,
+        districtPresidentSign,
+        profileImage: profileImage.filename, // Save the filename to the database
+      });
 
-    // Save the item to the database
-    await newItem.save();
+      // Save the item to the database
+      await newItem.save();
 
-    // Redirect to the home page or wherever you want
-    res.redirect('/');
-  } catch (error) {
-    // Handle errors (e.g., validation errors)
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+      // Redirect to the home page or wherever you want
+      res.redirect("/");
+    } catch (error) {
+      // Handle errors (e.g., validation errors)
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
   }
-});
-
+);
 
 // Edit route
-router.get('/edit/:id', isAuthenticated, async (req, res) => {
+router.get("/edit/:id", isAuthenticated, async (req, res) => {
   try {
     const itemId = req.params.id;
     const item = await Item.findById(itemId);
 
     if (!item) {
-      return res.status(404).send('Item not found');
+      return res.status(404).send("Item not found");
     }
 
-    res.render('edit', { item, user: req.user });
+    res.render("edit", { item, user: req.user });
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
-router.post('/edit/:id', upload.single('profileImage'), async (req, res) => {
+router.post("/edit/:id", upload.single("profileImage"), async (req, res) => {
   try {
     const itemId = req.params.id;
 
@@ -77,20 +128,40 @@ router.post('/edit/:id', upload.single('profileImage'), async (req, res) => {
     const item = await Item.findById(itemId);
 
     if (!item) {
-      return res.status(404).send('Item not found');
+      return res.status(404).send("Item not found");
     }
 
     // Update fields from the form
     item.name = req.body.name;
     item.fathername = req.body.fathername;
-    item.email = req.body.email;
+    item.contactNumber = req.body.contactNumber;
     item.district = req.body.district;
     item.constituencyType = req.body.constituencyType;
     item.constituency = req.body.constituency;
     item.cnic = req.body.cnic;
-    item.message = req.body.message;
-    item.question = req.body.question;
-
+    item.currentAddress = req.body.currentAddress;
+    item.permanentAddress = req.body.permanentAddress;
+    item.suggesterName= req.body.suggesterName;
+    item.suggesterFatherName= req.body.suggesterFatherName;
+    item.suggesterContactNum= req.body.suggesterContactNum;
+    item.suggesterCNIC= req.body.suggesterCNIC;
+    item.suggesterCurrentAddress= req.body.suggesterCurrentAddress;
+    item.suggConstTyp= req.body.suggConstTyp;
+    item.suggesterConstituency= req.body.suggesterConstituency;
+    item.vindicatorName= req.body.vindicatorName;
+    item.vindicatorFatherName= req.body.vindicatorFatherName;
+    item.vindicatorContactNum= req.body.vindicatorContactNum;
+    item.vindicatorCNIC= req.body.vindicatorCNIC;
+    item.vindicatorCurrentAddress= req.body.vindicatorCurrentAddress;
+    item.vindConstTyp= req.body.vindConstTyp;
+    item.vindicatorConstituency= req.body.vindicatorConstituency;
+    item.candidateSign= req.body.candidateSign;
+    item.suggesterSign= req.body.suggesterSign;
+    item.vindicatorSign= req.body.vindicatorSign;
+    item.ucPresidentSign= req.body.ucPresidentSign;
+    item.tehsilPresidentSign= req.body.tehsilPresidentSign;
+    item.districtPresidentSign= req.body.districtPresidentSign;
+    
     // If a new image is provided, update the imagePath
     if (req.file) {
       // Set both profileImage and imagePath to the new file name
@@ -98,27 +169,22 @@ router.post('/edit/:id', upload.single('profileImage'), async (req, res) => {
 
       item.profileImage = req.file.filename;
       // item.imagePath = req.file.filename;
-      item.imagePath = 'uploads/' + req.file.filename; // Ensure the correct path
+      item.imagePath = "uploads/" + req.file.filename; // Ensure the correct path
     }
 
     // Save the updated item
     await item.save();
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
-
-
-
-
-
 // Delete route
-router.get('/delete/:id', isAuthenticated, async (req, res) => {
+router.get("/delete/:id", isAuthenticated, async (req, res) => {
   await Item.findByIdAndDelete(req.params.id);
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
--- routes/upload.js
+++ routes/upload.js
@@ -1,43 +1,63 @@
 // routes/upload.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { upload } = require('../config/multer');
-const user = require('../models/User');
-const Item = require('../models/item');
-const isAuthenticated = require('./protected'); // Assuming this import is correct
+const { upload } = require("../config/multer");
+const user = require("../models/User");
+const Item = require("../models/item");
+const isAuthenticated = require("./protected"); // Assuming this import is correct
 
 // GET route to display the form with the image preview
-router.get('/add', isAuthenticated, (req, res) => {
+router.get("/add", isAuthenticated, (req, res) => {
   const user = req.user; // Fetch the user variable
-  res.render('add', { user });
+  res.render("add", { user });
 });
 
 // POST route for handling image upload and entry submission
-router.post('/add', upload.single('profileImage'), async (req, res) => {
+router.post("/add", upload.single("profileImage"), async (req, res) => {
   try {
     // Construct the relative file path without /public
-    const relativeFilePath = req.file.path.replace(/^public\//, '');
+    const relativeFilePath = req.file.path.replace(/^public\//, "");
 
     // Save the data to your MongoDB database using the Item model
     const newItem = new Item({
       name: req.body.name,
       fathername: req.body.fathername,
-      email: req.body.email,
+      contactNumber: req.body.contactNumber,
       district: req.body.district,
       constituencyType: req.body.constituencyType,
       constituency: req.body.constituency,
       cnic: req.body.cnic,
-      message: req.body.message,
-      question: req.body.question,
+      currentAddress: req.body.currentAddress,
+      permanentAddress: req.body.permanentAddress,
+      suggesterName: req.body.suggesterName,
+      suggesterFatherName: req.body.suggesterFatherName,
+      suggesterContactNum: req.body.suggesterContactNum,
+      suggesterCNIC: req.body.suggesterCNIC,
+      suggesterCurrentAddress: req.body.suggesterCurrentAddress,
+      suggConstTyp: req.body.suggConstTyp,
+      suggesterConstituency: req.body.suggesterConstituency,
+      vindicatorName: req.body.vindicatorName,
+      vindicatorFatherName: req.body.vindicatorFatherName,
+      vindicatorContactNum: req.body.vindicatorContactNum,
+      vindicatorCNIC: req.body.vindicatorCNIC,
+      vindicatorCurrentAddress: req.body.vindicatorCurrentAddress,
+      vindConstTyp: req.body.vindConstTyp,
+      vindicatorConstituency: req.body.vindicatorConstituency,
+      candidateSign: req.body.candidateSign,
+      suggesterSign: req.body.suggesterSign,
+      vindicatorSign: req.body.vindicatorSign,
+      ucPresidentSign: req.body.ucPresidentSign,
+      tehsilPresidentSign: req.body.tehsilPresidentSign,
+      districtPresidentSign: req.body.districtPresidentSign,
       imagePath: relativeFilePath, // Ensure 'imagePath' is a field in your Item model
     });
 
     await newItem.save();
     // Redirect to a success page or wherever you want after the entry is saved
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
